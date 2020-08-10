81becbff1c929aa894cab401e72e3359dfe925d1
Mushtaq Rossier <mushtaqmango16@gmail.com>
2020-08-10 19:04:43 +0200

    Create office-view components

---
+++ src/app/office-view/delete-staff/delete-staff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-staff',
+  templateUrl: './delete-staff.component.html',
+  styleUrls: ['./delete-staff.component.css']
+})
+export class DeleteStaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/office-view/office-view.module.ts
+++ src/app/office-view/office-view.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { OfficeViewRoutingModule } from './office-view-routing.module';
 import { OfficeViewComponent } from './office-view.component';
+import { CreateStaffComponent } from './create-staff/create-staff.component';
+import { DeleteStaffComponent } from './delete-staff/delete-staff.component';
+import { UpdateStaffComponent } from './update-staff/update-staff.component';
+import { ViewStaffComponent } from './view-staff/view-staff.component';
 
 
 @NgModule({
-  declarations: [OfficeViewComponent],
+  declarations: [OfficeViewComponent, CreateStaffComponent, DeleteStaffComponent, UpdateStaffComponent, ViewStaffComponent],
   imports: [
     CommonModule,
     OfficeViewRoutingModule
