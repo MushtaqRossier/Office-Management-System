755d134d8bbbe6b341aafa653a03ba4b276a83f0
Mushtaq Rossier <mushtaqmango16@gmail.com>
2020-08-11 20:54:07 +0200

    Updated view-office.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
+import { ViewOfficeComponent } from './home/view-office/view-office.component';
 
 
 
 const routes: Routes = [
   { path: 'office', loadChildren: () => import('./office-view/office-view.module').then(m => m.OfficeViewModule) },
-  { path: 'home', component: HomeModule},
+  { path: 'home', component: ViewOfficeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
