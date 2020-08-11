dda72555a63676afab069b3ebb450dd2c807c6f1
Mushtaq Rossier <mushtaqmango16@gmail.com>
2020-08-11 15:45:43 +0200

    Created UI template for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
 
-const routes: Routes = [{ path: 'office', loadChildren: () => import('./office-view/office-view.module').then(m => m.OfficeViewModule) }];
+
+const routes: Routes = [
+  { path: 'office', loadChildren: () => import('./office-view/office-view.module').then(m => m.OfficeViewModule) },
+  { path: 'home', component: HomeModule},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
