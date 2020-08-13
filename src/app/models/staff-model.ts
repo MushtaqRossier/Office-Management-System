287fd47359a2ed0f417a3c3ed1ee3c9018c2cfc7
Mushtaq Rossier <mushtaqmango16@gmail.com>
2020-08-13 10:09:27 +0200

    Updated model for staff data

--- src/app/models/staff-model.ts
+++ src/app/models/staff-model.ts
@@ -1,4 +1,6 @@
 export interface Staff {
+    Id: string;
+    OfficeId: String;
     FirstName: string;
     LastName: string;
 }
--- src/app/services/staff.service.ts
+++ src/app/services/staff.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Office } from 'src/app/models/office-view.model';
 import { Staff } from 'src/app/models/staff-model';
 import { Observable } from 'rxjs';
 
@@ -11,11 +10,6 @@ import { Observable } from 'rxjs';
 export class StaffService {
 
   constructor(private _http: HttpClient) { }
-
-  getOfficeViewData(): Observable<Office> {
-    const url = 'http://localhost:3000/officeDetails';
-    return this._http.get<Office>(url);
-  }
 
   getStaffData(): Observable<Staff> {
     const url = 'http://localhost:3000/staffDetails';
